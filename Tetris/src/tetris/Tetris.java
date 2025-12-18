0fe63cb7b9ccbb5c66bd8dbc59f56f467725f510
Samuel Bedeus <sambed405@gmail.com>
2025-12-18 19:20:50 +0100

    Creating the frame class
    Creating the frame class that will hold all the panels where the game window will later be added on

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -1,13 +1,9 @@
 
 package tetris;
 
-/**
- *
- * @author samue
- */
 public class Tetris {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        new Game();
     }
     
 }
